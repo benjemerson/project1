528d987e849d64113c23f72fed07fe487820dfb6
Ben Emerson <benemerson5@gmail.com>
2019-04-10 00:50:56 +0000

    fixed things

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
-int main() {
+//function declaration
+char movingTheAlphabet(char x, int locksmith);
 
+int main() {
+    // variables
+    int c;               //count for loop
+    char godDamnEnglishAlphabet[27] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};   //array for alphabet
+    //main code
+    //for (c = 0; c < 26; c++) {
+        printf("%c\n", movingTheAlphabet(godDamnEnglishAlphabet[1], -2));     // pritning the function of array
+   // }
   return 0;
 }
+
+//function definitions
+
+//substitution encode algorithm
+char movingTheAlphabet(char x, int locksmith) {
+    char shiftyMcShifted;
+    int positron;
+    if(locksmith < 0)
+        locksmith = -locksmith;
+    if ((x >= 65)){              
+        positron = x - 65;        
+        if (1 <= 26) {
+            shiftyMcShifted = (positron + locksmith)%26 + 65;
+        }
+    }   
+    return shiftyMcShifted;       //returns new value for new letter
+}
+
